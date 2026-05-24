b6e3c7ff01285c4e3c7ef1e4233d36ea5551b393
Habibur Rahman <habib@isoldien.com>
2026-05-24 18:12:59 +0100

    remove class files

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ name = input("Enter a driver's first name" + "\n")
 
 r = requests.get(f'https://api.openf1.org/v1/drivers?first_name={name}').json()
 
-print(r)
+if __name__ == "__main__":
+    print(r)
